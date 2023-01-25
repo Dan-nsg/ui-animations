5331dc1406e0c8b65f6a0d05fe0f48514b48296a
Daniel Giani <dnsgiani@gmail.com>
2023-01-25 13:18:05 -0300

    first commit

---
+++ Assets/Scripts/Buttons/ButtonScaler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonScaler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exit");
+    }
+}
